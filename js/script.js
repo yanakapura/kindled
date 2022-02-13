eb1bab7f8ec7034eadcf9e5cc22fee4cc17caafd
yanakapura <y.kapura@gmail.com>
2022-02-13 16:10:03 +0300

    Bugs are fixed, animated button is added

--- js/script.js
+++ js/script.js
@@ -9,18 +9,20 @@
 
 var lastScrollTop = 0;
 $(window).scroll(function (event) {
-  var st = $(this).scrollTop();
-  console.log(st);
-  if (st >= lastScrollTop) {
-    $(".menu").slideUp();
-  } else {
-    $(".menu").slideDown();
+  if ($(window).width() > 768) {
+    var st = $(this).scrollTop();
+    console.log(st);
+    if (st >= lastScrollTop) {
+      $(".menu").slideUp();
+    } else {
+      $(".menu").slideDown();
+    }
+    var alpha = Math.min(0.1 + (0.9 * $(this).scrollTop()) / 900, 0.9);
+    var channel = Math.round(alpha * 5);
+    $(".menu").css("background-color", "rgb(0,0,0,0." + channel + ")");
+    if (st <= 4) {
+      $(".menu").css("background-color", "rgb(0,0,0,0)");
+    }
+    lastScrollTop = st;
   }
-  var alpha = Math.min(0.1 + (0.9 * $(this).scrollTop()) / 900, 0.9);
-  var channel = Math.round(alpha * 5);
-  $(".menu").css("background-color", "rgb(0,0,0,0." + channel + ")");
-  if (st <= 4) {
-    $(".menu").css("background-color", "rgb(0,0,0,0)");
-  }
-  lastScrollTop = st;
 });
