ee3e5d3d78427539cbf6e0ed235a6e212ea892cb
yanakapura <y.kapura@gmail.com>
2022-02-13 10:53:49 +0300

    Added sliding menu

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// $(window).scroll(function (event) {
+//   if ($(this).scrollTop() > 80) {
+//     $(".menu").slideDown();
+//     return false;
+//   } else {
+//     $(".menu").slideUp();
+//   }
+// });
+
+var lastScrollTop = 0;
+$(window).scroll(function (event) {
+  var st = $(this).scrollTop();
+  console.log(st);
+  if (st >= lastScrollTop) {
+    $(".menu").slideUp();
+  } else {
+    $(".menu").slideDown();
+  }
+  var alpha = Math.min(0.1 + (0.9 * $(this).scrollTop()) / 900, 0.9);
+  var channel = Math.round(alpha * 5);
+  $(".menu").css("background-color", "rgb(0,0,0,0." + channel + ")");
+  if (st <= 4) {
+    $(".menu").css("background-color", "rgb(0,0,0,0)");
+  }
+  lastScrollTop = st;
+});
