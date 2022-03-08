46915311809520cf28929550a48a9b4a4cb7d276
yanakapura <y.kapura@gmail.com>
2022-03-08 21:42:43 +0300

    Collapsible menu is added

--- js/script.js
+++ js/script.js
@@ -7,6 +7,10 @@
 //   }
 // });
 
+document.body.addEventListener("click", (e) => {
+  console.log(e.target);
+});
+
 var lastScrollTop = 0;
 $(window).scroll(function (event) {
   if ($(window).width() > 768) {
@@ -37,8 +41,6 @@ function slidersAlexMari() {
       i < 3 ? ++i : (i = 1)
     }.jpg`;
 
-    console.log("changed");
-
     document.querySelector(
       "#slider-magazine"
     ).src = `../images/alex-mary/magazine-${j < 9 ? ++j : (j = 1)}.jpg`;
@@ -48,6 +50,32 @@ function slidersAlexMari() {
 }
 
 slidersAlexMari();
+
+document.querySelector(".menu-sm--icon").addEventListener("click", () => {
+  console.log("click");
+  document.querySelector(".menu-sm--icon").classList.toggle("active");
+  document.querySelector(".menu-sm--icon").classList.toggle("not-active");
+  document
+    .querySelector(".menu-sm--ul")
+    .classList.toggle("menu-sm--wrapper--down");
+});
+
+document
+  .querySelector(".menu-sm--projects-button")
+  .addEventListener("click", () => {
+    document
+      .querySelector(".menu-sm--projects-button-1")
+      .classList.toggle("rotate-90");
+    document
+      .querySelector(".menu-sm--projects-button-2")
+      .classList.toggle("rotate-180");
+    // document
+    //   .querySelector(".menu-sm--projects-ul")
+    //   .classList.toggle("display-block");
+    document
+      .querySelector(".menu-sm--projects-ul")
+      .classList.toggle("menu-sm--wrapper--down");
+  });
 
 document.querySelector("#projects").addEventListener("click", () => {
   console.log("hello");
